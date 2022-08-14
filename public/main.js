de11cc2d8fadcc348ee8c3cf32852224a03c21c9
paritomarrr <pari2019cse083@gmail.com>
2022-08-14 20:03:37 +0530

    delete option and styling

--- public/main.js
+++ public/main.js
@@ -1,13 +1,34 @@
-// main.js
-const update = document.querySelector('#update-button')
+/* globals fetch */
+var update = document.getElementById("update");
+var del = document.getElementById("delete");
 
-update.addEventListener('click', _ => {
-    fetch('/quotes', {
-        method: 'put',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            name: 'Darth Vader',
-            quote: 'I find your lack of faith disturbing.'
-          })
+update.addEventListener("click", function () {
+  fetch("quotes", {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: "Darth Vader",
+      quote: "I find your lack of faith disturbing.",
+    }),
+  })
+    .then((response) => {
+      if (response.ok) return response.json();
     })
-})
+    .then((data) => {
+      console.log(data);
+    });
+});
+
+del.addEventListener("click", function () {
+  fetch("quotes", {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name: "Darth Vader",
+    }),
+  }).then(function (response) {
+    window.location.reload();
+  });
+});
--- server.js
+++ server.js
@@ -1,59 +1,58 @@
-const express = require("express");
-const bodyParer = require("body-parser");
-const app = express();
-app.use(express.static('public'))
-app.use(bodyParer.json())
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
+var db
 
-
-const MongoClient = require("mongodb").MongoClient;
-
-var connectString =
-  "mongodb+srv://pari:FADFYrwi7EF1RW1a@cluster0.c2taoab.mongodb.net/?retryWrites=true&w=majority";
-
-MongoClient.connect(connectString, (err, client) => {
-  if (err) return console.error(err);
-  console.log("Connected to Database");
-  const db = client.db("CRUD");
-  const quotesCollection = db.collection('quotes')
-
-  app.use(bodyParer.urlencoded({ extended: true }));
-
- 
-
-  app.get("/",async function (req, res) {
-    // res.sendFile(__dirname + "/index.html");
-    const cursor = await db.collection('quotes').find().toArray()
-    console.log(cursor)
-    res.render('index.ejs', {quotes: cursor})
-    
-  });
-
-  app.listen(3000, function () {
-    console.log("Listening on port: 3000");
-  });
-
-  app.post("/quotes", async (req, res) => {
-   const insertQuote = await quotesCollection.insertOne(req.body)
-   console.log(insertQuote)
-   res.redirect('/')
-  });
-
-  app.put('/quotes', (req, res) => {
-    const quoteCollection = quotesCollection.findOneAndUpdate(
-        {name: 'Yoda'}, 
-        {
-    
-            $set: {
-                name: req.body.name,
-                quote: req.body.quote
-              }
-        }, 
-        {
-            upsert: true
-        }
-      )
-      console.log(quoteCollection)
+// Remember to change YOUR_USERNAME and YOUR_PASSWORD to your username and password! 
+MongoClient.connect("mongodb+srv://pari:FADFYrwi7EF1RW1a@cluster0.c2taoab.mongodb.net/?retryWrites=true&w=majority", (err, database) => {
+  if (err) return console.log(err)
+  db = database.db('star-wars-quotes')
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on 3000')
   })
+})
 
-});
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('index.ejs', {quotes: result})
+  })
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').insertOne(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes')
+  .findOneAndUpdate({name: 'Yoda'}, {
+    $set: {
+      name: req.body.name,
+      quote: req.body.quote
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+app.delete('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndDelete({name: req.body.name}, (err, result) => {
+    if (err) return res.send(500, err)
+    res.send('A darth vadar quote got deleted')
+  })
+})
